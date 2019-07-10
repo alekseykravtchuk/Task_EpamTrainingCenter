08cbc96b30e9e0d4c90a4e59d36fb0ceef63f654
Aliaksei Krauchuk <alekseykravtchuk@gmail.com>
2019-07-10 18:56:18 +0300

    done unit3.string_builder.t03_palindrome

--- src/by/krava/etc/unit3/string_builder/t03_palindrome/Palindrome.java
+++ src/by/krava/etc/unit3/string_builder/t03_palindrome/Palindrome.java
@@ -9,7 +9,9 @@ public class Palindrome {
     private static boolean isPalindrome (String word) {
         boolean isPalindrome = true;
         for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+            String a = String.valueOf(word.charAt(i));
+            String b = String.valueOf(word.charAt(word.length() - i - 1));
+            if (!a.equalsIgnoreCase(b)) {
                 isPalindrome = false;
             }
         }
@@ -17,7 +19,7 @@ public class Palindrome {
     }
 
     public static void main(String[] args) {
-        String [] words = {"palindrome", "madam", "level", "referee"};
+        String [] words = {"palindrome", "madam", "level", "referee", "Stanley Yelnats"};
         for (String word : words) {
             String result = isPalindrome(word) ? " - palindrome" : " - not palindrome";
             System.out.println(word + result);
