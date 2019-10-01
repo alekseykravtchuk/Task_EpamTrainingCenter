97bfc15b45e7e5dbac3a106ddba8cde618a58f7b
Aliaksei Krauchuk <alekseykravtchuk@gmail.com>
2019-10-01 21:04:49 +0300

    done unit5.t3_calendar

--- src/by/krava/etc/unit5/t3_calendar/Main.java
+++ src/by/krava/etc/unit5/t3_calendar/Main.java
@@ -2,7 +2,6 @@ package by.krava.etc.unit5.t3_calendar;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 
 //Создать класс Календарь с внутренним классом, с помощью объектов которого можно хранить информацию о
@@ -10,17 +9,18 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Calendar calendar = new Calendar(2020);
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy", new Locale("be", "BY"));
+        int year = 2020;
+        Calendar calendar = new Calendar(year);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-        calendar.addHoliday(LocalDate.parse("01.01.2020", dtf), "New Year");
-        calendar.addHoliday(LocalDate.parse("07.01.2020", dtf), "Orthodox Christmas");
-        calendar.addHoliday(LocalDate.parse("08.03.2020", dtf), "Women’s Day");
-        calendar.addHoliday(LocalDate.parse("01.05.2020", dtf), "Labour Day");
-        calendar.addHoliday(LocalDate.parse("09.05.2020", dtf), "Victory Day");
-        calendar.addHoliday(LocalDate.parse("03.07.2020", dtf), "Independence Day");
-        calendar.addHoliday(LocalDate.parse("07.11.2020", dtf), "October Revolution Day");
-        calendar.addHoliday(LocalDate.parse("25.12.2020", dtf), "Catholic Christmas");
+        calendar.addHoliday(LocalDate.parse("01.01." + year, dtf), "New Year");
+        calendar.addHoliday(LocalDate.parse("07.01." + year, dtf), "Orthodox Christmas");
+        calendar.addHoliday(LocalDate.parse("08.03." + year, dtf), "Women’s Day");
+        calendar.addHoliday(LocalDate.parse("01.05." + year, dtf), "Labour Day");
+        calendar.addHoliday(LocalDate.parse("09.05." + year, dtf), "Victory Day");
+        calendar.addHoliday(LocalDate.parse("03.07." + year, dtf), "Independence Day");
+        calendar.addHoliday(LocalDate.parse("07.11." + year, dtf), "October Revolution Day");
+        calendar.addHoliday(LocalDate.parse("25.12." + year, dtf), "Catholic Christmas");
 
         calendar.printHolidays();
 
